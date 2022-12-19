14dfa42cd99947cc5da930c401a36ae910f2e18a
sshrik <chj878194@naver.com>
2022-12-19 21:11:52 +0900

    feat: add temperature pages

---
+++ src/pages/SearchPasswordPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchPasswordPage: React.FC = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      비밀번호 찾기 페이지
+    </div>
+  );
+};
+
+export default SearchPasswordPage;
